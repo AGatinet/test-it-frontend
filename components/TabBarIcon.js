5788b879482ba23b36bad04bde392389c365e7ce
antoine gatinet <antoinegatinet@air-de-antoine.lan>
2018-12-06 18:51:31 +0100

    navbar finition

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Icon } from 'expo';
+import React from "react";
+// import { Icon } from "expo";
 
-import Colors from '../constants/Colors';
+import Colors from "../constants/Colors";
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
+import icoMoonConfig from "../assets/fonts/selection.json";
+const Icon = createIconSetFromIcoMoon(icoMoonConfig, "icomoon");
 
 export default class TabBarIcon extends React.Component {
   render() {
     return (
-      <Icon.Ionicons
+      <Icon
         name={this.props.name}
         size={26}
         style={{ marginBottom: -3 }}
-        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        color={
+          this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault
+        }
       />
     );
   }
